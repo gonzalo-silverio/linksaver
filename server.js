5d5d266bf1076bb43e7cae5b99595feeb0bbc9db
Gonzalo Silverio <gonzalo.silveriooo@gmail.com>
2020-08-05 21:43:29 -0500

    Se configura app para Heroku 3

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-onst express = require('express');
+const express = require('express');
+const http = require('http')
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
