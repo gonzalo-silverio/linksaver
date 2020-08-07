5bf9726b35e5fbed5a4f846666b19275eca20ca5
Gonzalo Silverio <gonzalo.silveriooo@gmail.com>
2020-08-07 17:15:54 -0500

    Se actualiza server.js 2

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.route('*', (req, res) => {
-  res.redirect(__dirname + '/dist/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 const port = process.env.PORT || 8080;
