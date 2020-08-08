c18e48f9b5c4553f3994f6360124a0444f08176f
Gonzalo Silverio <gonzalo.silveriooo@gmail.com>
2020-08-07 20:12:52 -0500

    Se agrega angular routing 7

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const path = require('path');
 
 const app = express();
 
-//app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static(${__dirname}/linksaver-app/dist/));
+app.use(express.static(path.join(__dirname, '/dist')));
 
 app.get('*',(req, res) => {
-  //res.sendFile(path.join(__dirname+'/dist/index.html'));
-  res.sendFile(`./linksaver-app/dist/index.html`);
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 
