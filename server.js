b7df9825b6a9bbe661db810c7130702a4fb0bdef
Gonzalo Silverio <gonzalo.silveriooo@gmail.com>
2020-08-07 20:01:46 -0500

    Se agrega angular routing 5

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 //app.use(express.static(path.join(__dirname, '/dist')));
-app.use(express.static(${__dirname}/dist/));
+app.use(express.static(${__dirname}/linksaver/dist/));
 
 app.get('*',(req, res) => {
   //res.sendFile(path.join(__dirname+'/dist/index.html'));
-  res.sendFile(`./dist/index.html`); 
+  res.sendFile(`./linksaver/dist/index.html`);
 });
 
 
