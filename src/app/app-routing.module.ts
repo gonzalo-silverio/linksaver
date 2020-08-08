416a367dfc6e1a092b462a639356d4a860ce1548
Gonzalo Silverio <gonzalo.silveriooo@gmail.com>
2020-08-07 19:35:13 -0500

    Se agrega angular routing 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 
@@ -8,10 +7,6 @@ const routes: Routes = [
   { path: '', component: AppComponent },
   { path: 'acerca', component: AboutComponent }  
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> 508a9b6e579d9e9286b56b6004a55d5e5d191fc7
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
